30f0761160642c7c039d0192c2288079bcf18285
kendrabrown82 <64144307+kendrabrown82@users.noreply.github.com>
2020-11-21 09:18:29 -0500

    Update Purchase.js
    boiler plate form

--- src/components/pages/Purchase.js
+++ src/components/pages/Purchase.js
@@ -5,13 +5,25 @@ import React from 'react';
 
 export default function Purchase () {
 
-    return <div className='purchase'>
+    return ( <div className='purchase'>
             <h1>Please fill out the form below to place your order!</h1>
             <center>
                 <p>10% of your purchase will be donated to <i>Girls Who Code</i>Thank You!</p>
             </center>
-           
-        </div>
+            <p></p>   <form>
+            <label>
+               First Name:
+    <input type="text" name="name" />
+
+            </label>
+            <label>
+               <br></br> Last Name: 
+    <input type="text" name="name" />
+    Email: <input type="text" name="name" /> <p></p>
+    Address: <input type="text" name="name" /> 
+            </label>
+            <input type="submit" value="Submit" />
+        </form> </div>)
         
             
         
