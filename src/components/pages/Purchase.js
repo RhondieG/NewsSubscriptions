b9282f658d060600e8c0f24a8055a3f76383d800
Kendra Choice <kendrabrown82@yahoo.com>
2020-11-21 11:29:38 -0500

    function updates to allow submit save DB

--- src/components/pages/Purchase.js
+++ src/components/pages/Purchase.js
@@ -1,30 +1,40 @@
 import React from 'react';
+import { useHistory } from "react-router-dom";
 
 
 
 
-export default function Purchase () {
+export default function Purchase() {
+    let history = useHistory();
 
-    return ( <div className='purchase'>
-            <h1>Please fill out the form below to place your order!</h1>
-            <center>
-                <p>10% of your purchase will be donated to <i>Girls Who Code</i>Thank You!</p>
-            </center>
-            <p></p>   <form>
+const handlesubmit = (event) =>{
+
+    event.preventDefault();
+    // submit form to backend using event.target.inputname.value
+    console.log(event.target.last_name.value)
+    history.push('/chat')
+}
+
+    return (<div className='purchase'>
+        <h1>Please fill out the form below to place your order!</h1>
+        <center>
+            <p>10% of your purchase will be donated to <i>Girls Who Code</i>Thank You!</p>
+        </center>
+        <p></p>   
+        <form onSubmit={handlesubmit}> 
             <label>
-               First Name:
-    <input type="text" name="name" />
-
-            </label>
+                First Name:
+    <input type="text" name="first_name" />
+    </label>
             <label>
-               <br></br> Last Name: 
-    <input type="text" name="name" />
-    Email: <input type="text" name="name" /> <p></p>
-    Address: <input type="text" name="name" /> 
+                <br></br> Last Name:
+    <input type="text" name="last_name" />
+    Email: <input type="text" name="email" /> <p></p>
+    Address: <input type="text" name="address" />
             </label>
             <input type="submit" value="Submit" />
         </form> </div>)
-        
-            
-        
+
+
+
 };
--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import '../../App.css';
 
-export default function SignUp () {
 
-    return<h1 className='signUp'>SIGN UP TO CHAT</h1>
-       
+    export default function SignUp() {
+        return (<div> <h1 className='signUp'>SIGN UP</h1>
+         <p></p>   <form>
+                <label>
+                   First Name:
+        <input type="text" name="name" />
+    
+                </label>
+                <label>
+                   <br></br> Last Name: 
+        <input type="text" name="name" />
+        Email: <input type="text" name="name" />
+        Password: <input type="text" name="name" /> 
+                </label>
+                <input type="submit" value="Submit" />
+                
+            </form> </div>)
+    
 
 
     //submit button on signUp page should lead to Chat page
