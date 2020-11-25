71df5b4de509b889bbf151f8d5fd5d7df243b514
Rhonda Gilliard <amourg08@gmail.com>
2020-11-24 19:39:11 -0500

    heroku

--- server/db.js
+++ server/db.js
@@ -2,10 +2,10 @@ require("dotenv").config();
 const Pool = require("pg").Pool;
 
 const pool = new Pool ({
-    user: "postgres",
-    password: "password",
+    user: "pwknknrlranilf",
+    password: "70a45abeec802e456a7b22f255d0f0d306ef44747f5be8adbb53bd7164e0e077",
     localhost: 5432,
-    database: "chatterbox"
+    database: "d3sa84srlmjbjg"
 });
 
 module.exports = pool;
